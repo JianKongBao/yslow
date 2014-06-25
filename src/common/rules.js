bde13344ae03159a3abef6d5126e93d489fae1af
neeke <ciogao@gmail.com>
2014-06-25 12:02:22 +0800

    add fastcdn.com

--- src/common/rules.js
+++ src/common/rules.js
@@ -119,7 +119,8 @@ YSLOW.registerRule({
             'cdn.api.twitter.com', //Twitter API calls, served via Akamai
             'apis.google.com', //Google's API Hosting
             '.akamaihd.net', //Akamai - Facebook uses this for SSL assets
-            '.rackcdn.com' //Generic RackSpace CloudFiles CDN
+            '.rackcdn.com', //Generic RackSpace CloudFiles CDN
+            '.fastcdn.com' //帝联cdn @modify author Neeke
         ],
         // array of regexps that will be treated as exception.
         exceptions: [
