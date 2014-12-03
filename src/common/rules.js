e5769750f73f7c5f4048c98c2c1d7802f6ad42b0
Steven <stevenliuit@gmail.com>
2014-12-03 17:19:57 +0800

    Update rules.js
    add cdn header

--- src/common/rules.js
+++ src/common/rules.js
@@ -136,7 +136,8 @@ YSLOW.registerRule({
         servers: [
             'cloudflare-nginx', // not using ^ and $ due to invisible
             'Cdn Cache Server', //网宿CDN @modify author Steven
-            'Powered-By-ChinaCache'//蓝讯CDN @modify author Steven
+            'Powered-By-ChinaCache',//蓝讯CDN @modify author Steven
+            'HIT from'//Other CDN @modify author Steven
         ],
         // which component types should be on CDN
         types: ['js', 'css', 'image', 'cssimage', 'flash', 'favicon']
